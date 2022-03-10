a20336f740e79fcbb23a17c08b9ca0a14ad7f330
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-10 14:18:30 +0000

    Added a progress bar

---
+++ lib/widgets/progress_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+class ProgressBar extends StatelessWidget {
+  ProgressBar({Key? key, required this.percentWatched}) : super(key: key);
+
+  double percentWatched = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return LinearPercentIndicator(
+      barRadius: const Radius.circular(10),
+      lineHeight: 3,
+      percent: percentWatched,
+      progressColor: Colors.grey[400],
+      backgroundColor: Colors.grey[600],
+    );
+  }
+}
