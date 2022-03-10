ec38efc3ac2f69d52f9f14f8e2bfc0ab0a01844c
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-10 14:20:53 +0000

    Implented a stories grid

---
+++ lib/widgets/stories_grid.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/providers/user_stories.dart';
+import '../widgets/story_item.dart';
+import 'package:provider/provider.dart';
+
+class StoriesGrid extends StatelessWidget {
+  const StoriesGrid({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final storiesData = Provider.of<UserStories>(context);
+    final stories = storiesData.stories;
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: storiesData.stories.length,
+      itemBuilder: (context, i) => StoryItem(
+        images: stories[i].images,
+        profileImageUrl: stories[i].profileImage,
+        name: stories[i].title,
+      ),
+    );
+  }
+}
