9803be0f264b4ad045045ede48d063eb31037727
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-10 14:10:23 +0000

    Added a setup of Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/providers/user_stories.dart';
+import 'package:instagram_clone/screens/story_screen.dart';
+import 'package:provider/provider.dart';
 import './screens/home_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData.light(),
-    home: const HomeScreen(),
+  runApp(ChangeNotifierProvider(
+    create: (context) => UserStories(),
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      routes: {
+        StoryScreen.id: (ctx) => const StoryScreen(),
+      },
+      home: const HomeScreen(),
+    ),
   ));
 }
