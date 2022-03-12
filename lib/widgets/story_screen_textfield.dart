246819c2b7edcc05bfff8aad4ce3ae5c60d140be
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-12 01:52:12 +0000

    Extracted story screen text field

---
+++ lib/widgets/story_screen_textfield.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class StoryScreenTextField extends StatelessWidget {
+  const StoryScreenTextField({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: Row(children: [
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.only(right: 20, left: 20),
+            height: 35,
+            child: TextField(
+              decoration: InputDecoration(
+                labelStyle: const TextStyle(color: Colors.white),
+                labelText: 'Send message',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.white, width: 0.7),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.white, width: 0.7),
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+              ),
+            ),
+          ),
+        ),
+        const Icon(
+          Icons.favorite_outline,
+          color: Colors.white,
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        const Icon(
+          Icons.message_outlined,
+          color: Colors.white,
+        ),
+        const SizedBox(
+          width: 20,
+        )
+      ]),
+    );
+  }
+}
