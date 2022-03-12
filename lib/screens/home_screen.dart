cb63e1cbb1045bc400ff8996fd513620dd9feff5
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-12 11:57:53 +0000

    Updated story item

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,6 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
--- lib/widgets/story_item_container.dart
+++ lib/widgets/story_item_container.dart
@@ -1,46 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/widgets/personal_story_item.dart';
 import 'package:outline_gradient_button/outline_gradient_button.dart';
+import '../screens/story_screen.dart';
 
-class StoryItemContainer extends StatelessWidget {
-  const StoryItemContainer({
+class StoryItemContainer extends StatefulWidget {
+  StoryItemContainer({
     Key? key,
     required this.profileImageUrl,
-    required this.watchedAllStories,
     required this.id,
   }) : super(key: key);
 
   final String profileImageUrl;
-  final Object? watchedAllStories;
+  Object? watchedAllStories;
   final String id;
 
   @override
+  State<StoryItemContainer> createState() => _StoryItemContainerState();
+}
+
+class _StoryItemContainerState extends State<StoryItemContainer> {
+  @override
   Widget build(BuildContext context) {
-    return id == 'm1'
+    return widget.id == 'm1'
         ? PersonalStoryItem(
-            profileImage: profileImageUrl,
+            profileImage: widget.profileImageUrl,
           )
-        : OutlineGradientButton(
-            child: SizedBox(
-              height: 50,
-              width: 50,
-              child: Center(
-                child: CircleAvatar(
-                  radius: 21.2,
-                  backgroundImage: NetworkImage(profileImageUrl),
+        : GestureDetector(
+            onTap: () {
+              Navigator.of(context)
+                  .pushNamed(StoryScreen.id, arguments: widget.id)
+                  .then((watchedEverything) {
+                setState(() {
+                  widget.watchedAllStories = watchedEverything;
+                });
+              });
+            },
+            child: OutlineGradientButton(
+              child: SizedBox(
+                height: 50,
+                width: 50,
+                child: Center(
+                  child: CircleAvatar(
+                    radius: 21.2,
+                    backgroundImage: NetworkImage(widget.profileImageUrl),
+                  ),
                 ),
               ),
+              gradient: LinearGradient(
+                colors: widget.watchedAllStories == true
+                    ? [Colors.grey, Colors.grey]
+                    : [Colors.pink, Colors.yellow],
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+              ),
+              strokeWidth: 2,
+              padding: EdgeInsets.zero,
+              radius: const Radius.circular(35),
             ),
-            gradient: LinearGradient(
-              colors: watchedAllStories == true
-                  ? [Colors.grey, Colors.grey]
-                  : [Colors.pink, Colors.yellow],
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-            ),
-            strokeWidth: 2,
-            padding: EdgeInsets.zero,
-            radius: const Radius.circular(35),
           );
   }
 }
