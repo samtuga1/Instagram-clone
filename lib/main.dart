432f7ad91c71700feec8e02258d0be8b17128bc2
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-12 10:15:28 +0000

    Allowed main file to receive multiple providers

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,15 @@ import 'package:provider/provider.dart';
 import './screens/home_screen.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (context) => UserStories(),
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (context) => UserStories(),
+      ),
+    ],
     child: MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
+      theme: ThemeData(),
       routes: {
         StoryScreen.id: (ctx) => const StoryScreen(),
       },
