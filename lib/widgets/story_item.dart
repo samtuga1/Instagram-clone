7f1670826e3c3c6ba14b8b4bde96a2d5ef5b78e4
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-12 10:41:09 +0000

    Received a future after poping from story screen

--- lib/widgets/story_item.dart
+++ lib/widgets/story_item.dart
@@ -4,27 +4,40 @@ import '../screens/story_screen.dart';
 import '../widgets/story_item_container.dart';
 import 'package:provider/provider.dart';
 
-class StoryItem extends StatelessWidget {
+class StoryItem extends StatefulWidget {
   final String id;
+  Object? watchedAll;
   StoryItem({
     Key? key,
     required this.id,
+    this.watchedAll,
   }) : super(key: key);
 
   @override
+  State<StoryItem> createState() => _StoryItemState();
+}
+
+class _StoryItemState extends State<StoryItem> {
+  @override
   Widget build(BuildContext context) {
-    final story = Provider.of<UserStories>(context).findById(id);
+    final story = Provider.of<UserStories>(context).findById(widget.id);
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
       child: Column(
         children: [
           GestureDetector(
             onTap: () {
-              Navigator.of(context).pushNamed(StoryScreen.id, arguments: id);
+              Navigator.of(context)
+                  .pushNamed(StoryScreen.id, arguments: widget.id)
+                  .then((watchedEverything) {
+                setState(() {
+                  widget.watchedAll = watchedEverything;
+                });
+              });
             },
             child: StoryItemContainer(
+              watchedAllStories: widget.watchedAll,
               profileImageUrl: story.profileImage,
-              watchedAllStories: story.watchedAll,
             ),
           ),
           const SizedBox(
