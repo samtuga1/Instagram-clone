3b354aa73e5d42260712848d17cb99fb5f3ff6e5
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-12 01:12:54 +0000

    Updates

--- lib/widgets/story_item.dart
+++ lib/widgets/story_item.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 import '../screens/story_screen.dart';
-import 'package:outline_gradient_button/outline_gradient_button.dart';
+import '../widgets/story_item_container.dart';
 
-class StoryItem extends StatelessWidget {
+class StoryItem extends StatefulWidget {
   final String id;
   final String profileImageUrl;
   final String name;
   final List<String> images;
-  const StoryItem({
+  StoryItem({
     Key? key,
     required this.id,
     required this.images,
     required this.profileImageUrl,
     required this.name,
   }) : super(key: key);
+
+  @override
+  State<StoryItem> createState() => _StoryItemState();
+}
+
+class _StoryItemState extends State<StoryItem> {
+  Object? _watchedAllStories = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,33 +30,22 @@ class StoryItem extends StatelessWidget {
         children: [
           GestureDetector(
             onTap: () {
-              Navigator.of(context).pushNamed(StoryScreen.id, arguments: id);
+              Navigator.of(context)
+                  .pushNamed(StoryScreen.id, arguments: widget.id)
+                  .then((watchedAll) => {
+                        setState(() {
+                          _watchedAllStories = watchedAll;
+                        })
+                      });
             },
-            child: OutlineGradientButton(
-              child: SizedBox(
-                height: 50,
-                width: 50,
-                child: Center(
-                  child: CircleAvatar(
-                    radius: 21.2,
-                    backgroundImage: NetworkImage(profileImageUrl),
-                  ),
-                ),
-              ),
-              gradient: const LinearGradient(
-                colors: [Colors.pink, Colors.yellow],
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-              ),
-              strokeWidth: 2,
-              padding: EdgeInsets.zero,
-              radius: const Radius.circular(35),
-            ),
+            child: StoryItemContainer(
+                profileImageUrl: widget.profileImageUrl,
+                watchedAllStories: _watchedAllStories),
           ),
           const SizedBox(
             height: 5,
           ),
-          Text(name)
+          Text(widget.name)
         ],
       ),
     );
