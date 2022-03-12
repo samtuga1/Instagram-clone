734e4cd8acb0b4c6a6e62c018e206411c9aea341
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-12 11:37:47 +0000

    Add an if statment show personal story item

--- lib/widgets/story_item_container.dart
+++ lib/widgets/story_item_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/widgets/personal_story_item.dart';
 import 'package:outline_gradient_button/outline_gradient_button.dart';
 
 class StoryItemContainer extends StatelessWidget {
@@ -6,34 +7,40 @@ class StoryItemContainer extends StatelessWidget {
     Key? key,
     required this.profileImageUrl,
     required this.watchedAllStories,
+    required this.id,
   }) : super(key: key);
 
   final String profileImageUrl;
   final Object? watchedAllStories;
+  final String id;
 
   @override
   Widget build(BuildContext context) {
-    return OutlineGradientButton(
-      child: SizedBox(
-        height: 50,
-        width: 50,
-        child: Center(
-          child: CircleAvatar(
-            radius: 21.2,
-            backgroundImage: NetworkImage(profileImageUrl),
-          ),
-        ),
-      ),
-      gradient: LinearGradient(
-        colors: watchedAllStories == true
-            ? [Colors.grey, Colors.grey]
-            : [Colors.pink, Colors.yellow],
-        begin: Alignment.topRight,
-        end: Alignment.bottomLeft,
-      ),
-      strokeWidth: 2,
-      padding: EdgeInsets.zero,
-      radius: const Radius.circular(35),
-    );
+    return id == 'm1'
+        ? PersonalStoryItem(
+            profileImage: profileImageUrl,
+          )
+        : OutlineGradientButton(
+            child: SizedBox(
+              height: 50,
+              width: 50,
+              child: Center(
+                child: CircleAvatar(
+                  radius: 21.2,
+                  backgroundImage: NetworkImage(profileImageUrl),
+                ),
+              ),
+            ),
+            gradient: LinearGradient(
+              colors: watchedAllStories == true
+                  ? [Colors.grey, Colors.grey]
+                  : [Colors.pink, Colors.yellow],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+            ),
+            strokeWidth: 2,
+            padding: EdgeInsets.zero,
+            radius: const Radius.circular(35),
+          );
   }
 }
