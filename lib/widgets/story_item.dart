c51dadcf8e4bbe3ac610b8369f7b50b306be787c
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-12 10:20:10 +0000

    Updated story item

--- lib/widgets/story_item.dart
+++ lib/widgets/story_item.dart
@@ -1,51 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/providers/user_stories.dart';
 import '../screens/story_screen.dart';
 import '../widgets/story_item_container.dart';
+import 'package:provider/provider.dart';
 
-class StoryItem extends StatefulWidget {
+class StoryItem extends StatelessWidget {
   final String id;
-  final String profileImageUrl;
-  final String name;
-  final List<String> images;
   StoryItem({
     Key? key,
     required this.id,
-    required this.images,
-    required this.profileImageUrl,
-    required this.name,
   }) : super(key: key);
 
   @override
-  State<StoryItem> createState() => _StoryItemState();
-}
-
-class _StoryItemState extends State<StoryItem> {
-  Object? _watchedAllStories = false;
-
-  @override
   Widget build(BuildContext context) {
+    final story = Provider.of<UserStories>(context).findById(id);
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
       child: Column(
         children: [
           GestureDetector(
             onTap: () {
-              Navigator.of(context)
-                  .pushNamed(StoryScreen.id, arguments: widget.id)
-                  .then((watchedAll) => {
-                        setState(() {
-                          _watchedAllStories = watchedAll;
-                        })
-                      });
+              Navigator.of(context).pushNamed(StoryScreen.id, arguments: id);
             },
             child: StoryItemContainer(
-                profileImageUrl: widget.profileImageUrl,
-                watchedAllStories: _watchedAllStories),
+              profileImageUrl: story.profileImage,
+              watchedAllStories: story.watchedAll,
+            ),
           ),
           const SizedBox(
             height: 5,
           ),
-          Text(widget.name)
+          Text(story.title)
         ],
       ),
     );
