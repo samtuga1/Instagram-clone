5f19f9cc2e499e386ffc21b74042e96d1883d338
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-16 15:33:11 +0000

    Update

--- lib/widgets/story_item.dart
+++ lib/widgets/story_item.dart
@@ -28,7 +28,7 @@ class StoryItem extends StatelessWidget {
           ),
           Text(
             story.title,
-            style: Theme.of(context).textTheme.bodyMedium,
+            style: Theme.of(context).textTheme.bodySmall,
           )
         ],
       ),
