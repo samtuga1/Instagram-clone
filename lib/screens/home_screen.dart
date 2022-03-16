8e059fe6e2a27d59717ab92dfcd20ccd22af4f06
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-16 15:32:05 +0000

    Updates

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,48 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/providers/user_post.dart';
+import 'package:instagram_clone/widgets/app_bar.dart';
+import 'package:provider/provider.dart';
 import '../widgets/stories_grid.dart';
 import '../widgets/user_post_item.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const id = '/home_screen';
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        AppBar(
-          backgroundColor: Colors.white,
-          //elevation: 1,
-          title: Text('Instagram',
-              style: Theme.of(context).textTheme.displayLarge),
-          actions: [
-            GestureDetector(
-              child: Image.asset(
-                'assets/icons/add.png',
-                scale: 2.4,
-              ),
-            ),
-            GestureDetector(
-              child: Image.asset(
-                'assets/icons/favorite.png',
-                scale: 1,
-              ),
-            ),
-            GestureDetector(
-              child: Image.asset(
-                'assets/icons/message.png',
-                scale: 1,
-              ),
-            ),
-          ],
-        ),
-        ListView(children: [
-          SizedBox(
-            height: 96,
-            child: StoriesGrid(),
-          ),
-        ]),
+        const HomeScreenBar(),
         Expanded(
-          child: UserPostItem(),
+          child: ListView(
+            children: [
+              const SizedBox(
+                height: 105,
+                child: StoriesGrid(),
+              ),
+              ChangeNotifierProvider(
+                create: ((context) => UserPosts()),
+                child: const UserPostItem(),
+              )
+            ],
+          ),
         )
       ],
     );
