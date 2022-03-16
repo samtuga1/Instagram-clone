e592937b3727ebba27466cf1a95f4b820c9ce4a1
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-16 15:33:48 +0000

    Updated my code

--- lib/widgets/user_post_item.dart
+++ lib/widgets/user_post_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/providers/user_post.dart';
+import 'package:provider/provider.dart';
 
 class UserPostItem extends StatelessWidget {
   const UserPostItem({
@@ -7,62 +9,111 @@ class UserPostItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final userPost = Provider.of<UserPosts>(context, listen: false);
     return ListView.builder(
-      itemCount: 1,
+      shrinkWrap: true,
+      itemCount: userPost.users.length,
       itemBuilder: (context, i) => Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ListTile(
-            leading: CircleAvatar(
-              radius: 15,
-            ),
-            title: Text('Samuel'),
-            subtitle: Text('Accra, Legon'),
-            trailing: Icon(Icons.more_horiz),
-          ),
-          Container(
-            height: 400,
-            color: Colors.blue,
-          ),
-          Row(
-            children: [
-              Row(
-                children: [
-                  Icon(Icons.favorite_border_outlined),
-                  Icon(Icons.favorite_border_outlined),
-                  Icon(Icons.favorite_border_outlined)
-                ],
-              ),
-              const Spacer(),
-              Icon(Icons.bookmark)
-            ],
-          ),
-          Text('49 likes'),
-          RichText(
-            text: TextSpan(
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                TextSpan(text: 'Samuel'),
-                TextSpan(text: 'Description')
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: CircleAvatar(
+                        backgroundImage:
+                            NetworkImage(userPost.users[i].profileImage),
+                      ),
+                    ),
+                    Text(
+                      userPost.users[i].name,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ],
+                ),
+                const Icon(Icons.more_horiz)
               ],
             ),
           ),
+          SizedBox(
+            height: 330,
+            child: Image.network(
+              userPost.users[i].postImage,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, right: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Icon(Icons.favorite_border),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Image.asset(
+                      'assets/icons/chat.png',
+                      scale: 1.4,
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Image.asset(
+                      'assets/icons/send.png',
+                      scale: 1.3,
+                    ),
+                  ],
+                ),
+                const Icon(Icons.bookmark_outline),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 10, top: 10, bottom: 8),
+            child: Text('${userPost.users[i].likes} likes',
+                style: Theme.of(context).textTheme.bodySmall),
+          ),
           Row(
             children: [
               Row(
                 children: [
+                  const SizedBox(
+                    width: 10,
+                  ),
                   CircleAvatar(
+                    backgroundImage:
+                        NetworkImage(userPost.users[i].profileImage),
                     radius: 10,
                   ),
-                  Text('Add a comment'),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const Text(
+                    'Add a comment...',
+                    style: TextStyle(color: Colors.grey),
+                  ),
                 ],
               ),
-              Spacer(),
-              RichText(
-                text: TextSpan(
-                  children: [TextSpan(text: '♥'), TextSpan(text: '🙌')],
-                ),
-              ),
+              // Spacer(),
+              // RichText(
+              //   text: TextSpan(
+              //     children: [TextSpan(text: '♥'), TextSpan(text: '🙌')],
+              //   ),
+              // ),
             ],
+          ),
+          const SizedBox(
+            height: 15,
           )
         ],
       ),
