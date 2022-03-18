c9c88090c84ef05d7def52ad06f7d82c4986e742
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-18 10:49:16 +0000

    Updated

--- lib/providers/user_post.dart
+++ lib/providers/user_post.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
-class UserPost {
-  final String id;
-  final String name;
-  final String profileImage;
-  final String postImage;
+class UserPost with ChangeNotifier {
+  final String? id;
+  final String? name;
+  final String? profileImage;
+  final String? postImage;
   bool isFavorite;
-  final int likes;
+  final int? likes;
 
   UserPost({
-      required this.likes,
-      required this.profileImage,
-      required this.id,
-      required this.name,
-      required this.postImage,
-      required this.isFavorite,
-      });
+    this.likes,
+    this.profileImage,
+    this.id,
+    this.name,
+    this.postImage,
+    this.isFavorite = false,
+  });
+  void switchFav(String? id, int i) {
+    if (UserPosts()._users[i].id == id) {
+      isFavorite = !isFavorite;
+      notifyListeners();
+    } else {
+      print('not');
+      print(UserPost().id);
+    }
+  }
 }
 
 class UserPosts with ChangeNotifier {
@@ -25,8 +34,7 @@ class UserPosts with ChangeNotifier {
       likes: 32,
       id: 'm1',
       name: 'Samuel',
-      profileImage:
-          'https://avatars.githubusercontent.com/u/79772304?v=4',
+      profileImage: 'https://avatars.githubusercontent.com/u/79772304?v=4',
       postImage:
           'https://user-images.githubusercontent.com/79772304/154733047-5dcb4c0d-4ff7-497e-9a66-28c2b765847a.png',
       isFavorite: false,
@@ -35,8 +43,7 @@ class UserPosts with ChangeNotifier {
       likes: 32,
       id: 'm1',
       name: 'Samuel',
-      profileImage:
-          'https://avatars.githubusercontent.com/u/79772304?v=4',
+      profileImage: 'https://avatars.githubusercontent.com/u/79772304?v=4',
       postImage:
           'https://user-images.githubusercontent.com/79772304/152646045-a3ffc16c-0e11-4606-9f17-52326fe9a7da.png',
       isFavorite: false,
@@ -45,8 +52,7 @@ class UserPosts with ChangeNotifier {
       likes: 32,
       id: 'm1',
       name: 'Samuel',
-      profileImage:
-          'https://avatars.githubusercontent.com/u/79772304?v=4',
+      profileImage: 'https://avatars.githubusercontent.com/u/79772304?v=4',
       postImage:
           'https://user-images.githubusercontent.com/79772304/150697981-a080220c-21ee-4b24-b10c-8b3371c8991d.png',
       isFavorite: false,
@@ -68,10 +74,7 @@ class UserPosts with ChangeNotifier {
 
   // void switchFav(String id) {
   //   final user = _users.firstWhere((userPost) => userPost.id == id);
-  //   if (user.id == id) {
-  //     user.isFavorite = !user.isFavorite;
-  //     notifyListeners();
-  //   }
+  //   user.isFavorite = !user.isFavorite;
   //   notifyListeners();
   // }
 }
