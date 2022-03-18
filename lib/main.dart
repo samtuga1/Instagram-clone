2f62df77a5118da157f89e782febb62346819b4d
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-18 10:49:03 +0000

    Added another provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:instagram_clone/providers/user_post.dart';
 import 'package:instagram_clone/screens/pages_screen.dart';
 import './providers/user_stories.dart';
 import './screens/story_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MultiProvider(
+  runApp(
+    MultiProvider(
     providers: [
       ChangeNotifierProvider(
         create: (context) => UserStories(),
+      ),
+      ChangeNotifierProvider<UserPost>(
+        create: (context) => UserPost(),
       ),
     ],
     child: MaterialApp(
