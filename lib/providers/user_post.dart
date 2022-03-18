8240dcf89ac82d74fe1e4fc8639ec61acd230b0b
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-03-18 12:50:40 +0000

    Updated

--- lib/providers/user_post.dart
+++ lib/providers/user_post.dart
@@ -17,14 +17,18 @@ class UserPost with ChangeNotifier {
     this.postImage,
     this.isFavorite = false,
   });
-  void switchFav(String? id, int i) {
-    if (UserPosts()._users[i].id == id) {
-      isFavorite = !isFavorite;
-      notifyListeners();
-    } else {
-      print('not');
-      print(UserPost().id);
-    }
+  void switchFav(String? id) {
+    UserPost user = UserPosts()._users.firstWhere((post) => post.id == id);
+    user.isFavorite = !user.isFavorite;
+    notifyListeners();
+    print(user.isFavorite);
+    // if (UserPosts()._users[i].id == id) {
+    //   isFavorite = !isFavorite;
+    //   notifyListeners();
+    // } else {
+    //   print('not');
+    //   print(UserPost().id);
+    // }
   }
 }
 
@@ -37,25 +41,22 @@ class UserPosts with ChangeNotifier {
       profileImage: 'https://avatars.githubusercontent.com/u/79772304?v=4',
       postImage:
           'https://user-images.githubusercontent.com/79772304/154733047-5dcb4c0d-4ff7-497e-9a66-28c2b765847a.png',
-      isFavorite: false,
     ),
     UserPost(
       likes: 32,
-      id: 'm1',
+      id: 'm2',
       name: 'Samuel',
       profileImage: 'https://avatars.githubusercontent.com/u/79772304?v=4',
       postImage:
           'https://user-images.githubusercontent.com/79772304/152646045-a3ffc16c-0e11-4606-9f17-52326fe9a7da.png',
-      isFavorite: false,
     ),
     UserPost(
       likes: 32,
-      id: 'm1',
+      id: 'm3',
       name: 'Samuel',
       profileImage: 'https://avatars.githubusercontent.com/u/79772304?v=4',
       postImage:
           'https://user-images.githubusercontent.com/79772304/150697981-a080220c-21ee-4b24-b10c-8b3371c8991d.png',
-      isFavorite: false,
     ),
     // UserPost(
     //   likes: 32,
